d1c4052a49d8ce1cbe888d02d55d321fd028fb70
Rustam Karimov <rkarimov.r@gmail.com>
2022-02-14 15:24:21 +0400

    complete trait tasks;

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,20 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut x = self.clone();
+        let mut bar = vec![String::from("Bar")];
+        x.append(&mut bar);
+        x
+    }
+}
 
 #[cfg(test)]
 mod tests {
